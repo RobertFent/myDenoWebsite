0442c56d3c1527da0e88de142357df4e8ba4074f
RobertFent <robert.fent@live.com>
2021-07-07 10:24:18 +0200

    added constant for version tag

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -17,4 +17,5 @@ export const HOST_NAME = '0.0.0.0';
 export const CONNECTION_STRING = 'mongodb://admin:admin@localhost:27017';
 export const DEFAULT_DB = 'dev-website';
 export const DEFAULT_LOG_LEVEL = LogLevel.DEBUG;
+export const VERSION_TAG = 'v0.x.x'
 export const MONGO_ATLAS = false;
