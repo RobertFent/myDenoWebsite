d8cc57fc7233eef846f39af466cd1c9fc2465373
RobertFent <robert.fent@live.com>
2021-07-08 17:30:12 +0200

    added x-real-ip

--- backend/src/controllers/mainPage.controller.ts
+++ backend/src/controllers/mainPage.controller.ts
@@ -8,6 +8,7 @@ import { Logger } from "../utils/logger.ts";
  * @param timestamp string from LastVisit cookie
  * @returns {boolean} true if last visit was not today
  */
+// deno-lint-ignore no-unused-vars
 const newVisitation = (timestamp: string): boolean => {
     if (!timestamp) return true;
     const lastVisitDay = getDayFromCustomTimestamp(timestamp);
@@ -21,13 +22,26 @@ const newVisitation = (timestamp: string): boolean => {
 // deno-lint-ignore no-explicit-any
 export const getMainPage = async (ctx: any) => {
     await ctx.render(`${pagesDir}/${PageInformation.MainPage.HtmlFile}`);
-    if (MongoClientWrapper.isConnected) {
+    
+    let realIp;
+    try {
+        realIp = ctx.request.headers.get('x-real-ip');
+        Logger.debug(import.meta.url, `x-real-ip: ${realIp}`);
+    } catch (error) {
+        Logger.error(import.meta.url, error);
+    }
+
+    const ip = realIp ? realIp : ctx.request.ip;
+    MongoClientWrapper.isConnected ? void MongoClientWrapper.insertVisitor(ip, generateTimestamp()) : Logger.info(import.meta.url, 'No connection to db!');
+    
+    // use code below if using cookie 'LastVisit'
+    /* if (MongoClientWrapper.isConnected) {
         // only insert same visitor once a day
         if (newVisitation(ctx.cookies.get('LastVisit'))) {
-            // todo await vs void
-            void MongoClientWrapper.insertVisitor(ctx.request.ip, generateTimestamp());
-        }
+            const ip = realIp ? realIp : ctx.request.ip;
+            void MongoClientWrapper.insertVisitor(ip, generateTimestamp());
+        }   
     } else {
-        Logger.debug(import.meta.url, 'No connection to db!');
-    }
+        Logger.info(import.meta.url, 'No connection to db!');
+    } */
 };
