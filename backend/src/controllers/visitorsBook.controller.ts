f3417d64cd865167b6fff92dded9d9ecf0c68610
RobertFent <robert.fent@live.com>
2021-07-09 16:55:22 +0200

    visitors can now only add 3 entries

--- backend/src/controllers/visitorsBook.controller.ts
+++ backend/src/controllers/visitorsBook.controller.ts
@@ -1,6 +1,6 @@
 import { VisitorEntry } from "../models/visitorEntry.d.ts"
 import { MongoClientWrapper } from "../utils/mongoClientWrapper.ts"
-import { generateTimestamp, pagesDir } from "../utils/utils.ts";
+import { generateTimestamp, getIp, pagesDir } from "../utils/utils.ts";
 import { RouterContext } from "../../deps.ts";
 import { PageInformation } from "../utils/constants.ts";
 import { Logger } from "../utils/logger.ts";
@@ -11,7 +11,8 @@ const parseVisitorEntry = async (ctx: RouterContext): Promise<VisitorEntry> => {
     return {
         name: formData.name,
         message: formData.message,
-        timestamp: generateTimestamp()
+        timestamp: generateTimestamp(),
+        ip: getIp(ctx)
     }
 }
 
@@ -19,9 +20,9 @@ const parseVisitorEntry = async (ctx: RouterContext): Promise<VisitorEntry> => {
 export const getVisitorsBook = async (ctx: any) => {
     // entries loaded when no db connection is available
     let existingEntries: VisitorEntry[] = [
-        { timestamp: generateTimestamp(), name: 'Test User 1', message: 'Test Entry 1'},
-        { timestamp: generateTimestamp(), name: 'Test User 2', message: 'Test Entry 2'},
-        { timestamp: generateTimestamp(), name: 'Test User 3', message: 'Test Entry 3'}
+        { timestamp: generateTimestamp(), name: 'Test User 1', message: 'Test Entry 1', ip: '1.2.3.4'},
+        { timestamp: generateTimestamp(), name: 'Test User 2', message: 'Test Entry 2', ip: '1.2.3.5'},
+        { timestamp: generateTimestamp(), name: 'Test User 3', message: 'Test Entry 3', ip: '1.2.3.6'}
     ];
     if (MongoClientWrapper.isConnected) {
         existingEntries = await MongoClientWrapper.getVisitorEntries();
@@ -31,14 +32,30 @@ export const getVisitorsBook = async (ctx: any) => {
     await ctx.render(`${pagesDir}/${PageInformation.VisitorsBook.HtmlFile}`, {entries: existingEntries, versionTag: versionTag});
 }
 
-// todo error handling?
 export const postVisitorEntry = async (ctx: RouterContext) => {
     const entry = await parseVisitorEntry(ctx);
-    if (MongoClientWrapper.isConnected) {
-        await MongoClientWrapper.insertVisitorEntry(entry);
-    } else {
-        Logger.debug(import.meta.url, 'No connection to db! Entry could not be added');
+
+    // value which decides of somebody should write in the book
+    let ableToWrite = true;
+
+    // check if visitor did more than three entries and set ability to false
+    const existingEntriesByIp = await MongoClientWrapper.getVisitorEntriesByIp(entry.ip);
+    Logger.debug(import.meta.url, `Current entries of visitor: ${JSON.stringify(existingEntriesByIp)}`)
+    if ((existingEntriesByIp && existingEntriesByIp.length > 3)) {
+        
+        Logger.debug(import.meta.url, `Visitor (${entry.ip}) already added three entries!`)
+        ableToWrite = false;
     }
+
+    // check if db is connected and set ability to false if not
+    if (!MongoClientWrapper.isConnected) {
+        Logger.debug(import.meta.url, 'No connection to db! Entry will not be added');
+        ableToWrite = false;
+    }
+
+    // add entry if everything is ok
+    if (ableToWrite) await MongoClientWrapper.insertVisitorEntry(entry);
+
     // reload site with new entry
     await getVisitorsBook(ctx);
 }
